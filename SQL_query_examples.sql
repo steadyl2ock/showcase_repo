636dcf3cf9e12333913a86e546c40ae217682f23
steadyl2ock <76068209+steadyl2ock@users.noreply.github.com>
2021-02-10 12:59:09 -0800

    more
    added more sql queries

--- SQL_query_examples.sql
+++ SQL_query_examples.sql
@@ -20,4 +20,57 @@ FROM
     JOIN playertable AS pt
         ON tp.player_id = pt.player_id
      GROUP BY tt.team_name) AS table_1
-GROUP BY table_1.team_name;
+GROUP BY table_1.team_name
+;
+
+
+
+SELECT
+     first_name,
+     last_name,
+     COUNT(rating) AS count,
+     IFNULL(MIN(rating), 0) AS min,
+     IFNULL(MAX(rating), 0) AS max,
+     ROUND(IFNULL(AVG(rating), 0) , 2) AS avg,
+     CASE 
+         WHEN COUNT(rating) >= 10 THEN 'POWER USER'
+         WHEN COUNT(rating) > 0 THEN 'ACTIVE'
+         ELSE 'INACTIVE'
+     END AS 'STATUS'
+ FROM reviewers
+ LEFT JOIN reviews
+     ON reviewers.id = reviews.reviewer_id
+ GROUP BY reviewers.id
+ ;
+ 
+ 
+ 
+ SELECT
+    photos.id AS id,
+    COUNT(photo_id) AS likes,
+    username
+FROM photos
+RIGHT JOIN likes
+    ON photos.id = likes.photo_id
+RIGHT JOIN users
+    ON photos.user_id = users.id
+GROUP BY
+    id
+ORDER BY
+    likes DESC
+LIMIT 3
+;
+
+
+
+SELECT
+    title,
+    rating,
+    CONCAT(first_name, ' ', last_name) AS reviewers
+FROM series
+INNER JOIN reviews
+    ON series.id = reviews.series_id
+INNER JOIN reviewers
+    ON reviewers.id = reviews.reviewer_id
+ORDER BY title
+;
